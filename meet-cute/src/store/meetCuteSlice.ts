669cfb07edaf559136e5a1b77ebe1717548cfcd6
Noy <noy@Noys-MacBook-Pro.local>
2021-12-06 13:01:22 +0200

    add new folders

---
+++ meet-cute/src/store/meetCuteSlice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const meetCuteSlice = createSlice({
+  name: "meetCute",
+  initialState: {},
+  reducers: {},
+});
+
+export const {} = meetCuteSlice.actions;
+
+export default meetCuteSlice.reducer;
