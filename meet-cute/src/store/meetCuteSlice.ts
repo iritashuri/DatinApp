5e4f6cb53be18ace4dd68589bdc7990e023d8420
Shir <shir@hiPhonelArielle.meetinplace>
2021-12-06 16:06:11 +0200

    initiation of store objects - user data typing and Gender classification

--- meet-cute/src/store/meetCuteSlice.ts
+++ meet-cute/src/store/meetCuteSlice.ts
@@ -1,8 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
+import IUserData from "../models/IUserData";
 
 export const meetCuteSlice = createSlice({
   name: "meetCute",
-  initialState: {},
+  initialState: {
+    isLoggedIn: false,
+    loginKey: '',
+    userData: {} as IUserData,
+    userMatches: Array<IUserData>(),
+    userSearches: Array<IUserData>(),
+    userCurrentConversation: {},
+    userCurrentConvPartnerToken: '',
+    currentProfilePageInfo: {} as IUserData,
+    currentProfileIsThisUser: false,
+  },
   reducers: {},
 });
 
