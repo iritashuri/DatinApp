cb224d315f257cbdfba4f971901de8a49b87ffb8
shir <shirmolina@gmail.com>
2021-12-06 16:26:04 +0200

    bla

--- meet-cute/src/store/meetCuteSlice.ts
+++ meet-cute/src/store/meetCuteSlice.ts
@@ -1,8 +1,8 @@
-import { createSlice } from "@reduxjs/toolkit";
-import IUserData from "../models/IUserData";
+import { createSlice } from '@reduxjs/toolkit';
+import IUserData from '../models/IUserData';
 
 export const meetCuteSlice = createSlice({
-  name: "meetCute",
+  name: 'meetCute',
   initialState: {
     isLoggedIn: false,
     loginKey: '',
@@ -14,7 +14,22 @@ export const meetCuteSlice = createSlice({
     currentProfilePageInfo: {} as IUserData,
     currentProfileIsThisUser: false,
   },
-  reducers: {},
+  reducers: {
+    login: (state, action) => {},
+
+    logout: (state, action) => {},
+
+    fetcheMatches: (state, action) => {},
+
+    fetchSearches: (state, action) => {},
+
+    fetchUserData: (state, action) => {},
+
+    // check if user profile i
+    fetchProfilePage: (state, action) => {},
+
+
+  },
 });
 
 export const {} = meetCuteSlice.actions;
