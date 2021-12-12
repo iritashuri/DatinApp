6dd7b2709c611a4929aae52f394628472a8d3c22
Noy <noy@Noys-MacBook-Pro.local>
2021-12-12 11:23:23 +0200

    add css file for form

--- meet-cute/src/components/assets/AppMainImg.tsx
+++ meet-cute/src/components/assets/AppMainImg.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+const gif = "https://c.tenor.com/PmdAE3m4CscAAAAi/lady-and-the-tramp-kiss.gif";
+
 const image =
   'https://syringe-test.s3.us-west-2.amazonaws.com/shir-project-meet-cute-description.png';
 
 const AppMainImg: React.FC = () => {
-  return <img src={image} alt='main image' />;
+  return <img src={gif} alt='main image' />;
 };
 
 export default AppMainImg;
